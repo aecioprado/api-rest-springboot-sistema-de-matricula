4f0c60897e2fff8cb0babb91723e4936aaf79d3a
Aecio Prado <aecioprado@gmail.com>
2022-05-26 14:36:54 -0300

    feat: adicionou método addRole

--- src/main/java/gestao/matriculas/domain/Usuario.java
+++ src/main/java/gestao/matriculas/domain/Usuario.java
@@ -29,6 +29,10 @@ public class Usuario implements Serializable {
     )
     private List<Role> roles;
 
+    public void addRole(Role role){
+        this.roles.add(role);
+    };
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
