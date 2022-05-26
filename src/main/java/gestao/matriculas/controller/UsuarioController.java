4597d0644926e278a0c34302eed4c9f5cc987879
Aecio Prado <aecioprado@gmail.com>
2022-05-26 14:35:09 -0300

    refact: renomeou endpoints e adicionou listagem paginada

--- src/main/java/gestao/matriculas/controller/UsuarioController.java
+++ src/main/java/gestao/matriculas/controller/UsuarioController.java
@@ -1,9 +1,12 @@
 package gestao.matriculas.controller;
 
 import gestao.matriculas.domain.Usuario;
+import gestao.matriculas.dto.UsuarioDTO;
 import gestao.matriculas.service.UsuarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,36 +20,39 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping("/listar")
-    public List<Usuario> listarUsuarios(){
+    @GetMapping()
+    public List<Usuario> list(){
         return usuarioService.getAll();
     }
 
-    @GetMapping("/listar/{usuarioId}")
-    public Usuario listarUsuarioPorId(@PathVariable("usuarioId") Long usuarioId) {
-        return usuarioService.getOneById(usuarioId).get();
+    @GetMapping("/pagination")
+    public Page<Usuario> paginatedList(
+    @RequestParam (value = "page", defaultValue = "0", required = false) Integer page,
+    @RequestParam (value = "size", defaultValue = "5", required = false) Integer size){
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+        //PageRequest pageRequest = PageRequest.of(Integer.parseInt(page), Integer.parseInt(size));
+        return usuarioService.getWithPagination(pageRequest);
     }
 
-    @PostMapping("/novo")
-    public Usuario criarUsuario(@RequestBody Usuario usuario) {
+    @GetMapping("/{usuarioId}")
+    public Usuario listById(@PathVariable("usuarioId") Long usuarioId) {
+        return usuarioService.getOneById(usuarioId);
+    }
+
+    @PostMapping()
+    public Usuario save(@RequestBody Usuario usuario) {
         return usuarioService.save(usuario);
     }
 
-    @PutMapping("/{usuarioId}/editar")
-    public Usuario editarUsuario(@PathVariable("usuarioId") Long usuarioId, @RequestBody Usuario usuarioEditado) {
-        return usuarioService.update(usuarioId, usuarioEditado);
+    @PutMapping("/{usuarioId}")
+    public Usuario edit(@PathVariable("usuarioId") Long usuarioId, @RequestBody UsuarioDTO usuarioDTO) {
+        return usuarioService.update(usuarioId, usuarioDTO);
     }
 
     @DeleteMapping("/{usuarioId}")
-    public String excluirUsuario(@PathVariable("usuarioId") Long usuarioId) {
-        var isRemoved = usuarioService.delete(usuarioId);
-
-        if(isRemoved){
-            return "Usuário id #"+usuarioId+" excluido com sucesso";
-        }
-        else{
-            return "Usuário não excluido";
-        }
+    public void delete(@PathVariable("usuarioId") Long usuarioId) {
+       usuarioService.delete(usuarioId);
     }
 
 
